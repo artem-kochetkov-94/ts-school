fd9eb83efa14d22fe611906525e25c7ccda0a0d6
artem-kochetkov-94 <artem-kochetkov-94@mail.ru>
2023-05-03 13:46:06 +0400

    test6

---
+++ my-app/src/test6.ts
@@ -0,0 +1,21 @@
+abstract class Logger {
+  abstract log(message: string): void;
+
+  printDate(date: Date) {
+    this.log(date.toString());
+  }
+}
+
+class MyLogger extends Logger {
+  log(message: string) {
+    console.log(message);
+  }
+
+  logWithDate(message: string) {
+    this.printDate(new Date());
+    this.log(message);
+  }
+}
+
+const log = new MyLogger();
+log.logWithDate("message");
