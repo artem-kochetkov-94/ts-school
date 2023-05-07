2250575f1117995964c4854ba93593e65241f4ff
artem-kochetkov-94 <artem-kochetkov-94@mail.ru>
2023-05-07 14:26:49 +0400

    ffmpeg upload

---
+++ command-executor/src/out/console-logger/console-logger.ts
@@ -0,0 +1,25 @@
+import { IStreamLogger } from "../../core/handlers/stream-logger.interface";
+
+export class ConsoleLogger implements IStreamLogger {
+  public static logger: ConsoleLogger;
+
+  public static getInstance() {
+    if (!ConsoleLogger.logger) {
+      ConsoleLogger.logger = new ConsoleLogger();
+    }
+
+    return ConsoleLogger.logger;
+  }
+
+  public log(...args: any[]) {
+    console.log(args);
+  }
+
+  error(...args: any[]): void {
+    console.log(args);
+  }
+
+  end(): void {
+    console.log("End");
+  }
+}
